2fe68c10171a7ab5bced65dcf7c34236d87700e4
Jiri Spac <capajj@gmail.com>
2016-02-14 00:47:57 +0100

    proper render call with a third argument containing firstChild

--- public/app.js
+++ public/app.js
@@ -1,18 +1,13 @@
 import 'systemjs-hot-reloader/default-listener.js'
 
-import preact, { h, render, Component } from 'preact'
+import { h, render, Component } from 'preact'
 
-// export class HelloWorld extends Component {
-//   render () {
-//     return h('h1', null, 'Hello World')
-//   }
-// }
+export class HelloWorld extends Component {
+  render () {
+    return h('h1', null, 'Hello World')
+  }
+}
 
 const container = document.getElementById('app')
-const original = container.cloneNode(true)
-render(h('h1', null, 'Hello World'), container)
 
-export function __unload () {
-	container.remove() // since preact appends newly rendered to into the container, we need to manually clean it or we get a duplicated DOM every rerender
-	document.body.appendChild(original.cloneNode(true))
-}
+render(h(HelloWorld), container, container.firstChild)
